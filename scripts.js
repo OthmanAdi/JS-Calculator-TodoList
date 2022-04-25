d416014a67de13c0e9ef70223c0729d22f16a4a9
OthmanAdi <adiatwork@outlook.com>
2022-04-25 12:41:26 +0200

    newUpdate: Calculator html body + JS & new TodoList project

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var calculator = document.querySelector(".calculatorKeys");
+const key = calculator.querySelector(".calculatorBody");
+
+key.addEventListener("click", e =>{
+    if (e.target.matches('div')){
+        console.log("IM ALIVE");
+    }
+})
